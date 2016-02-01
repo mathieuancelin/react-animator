1e4d982eef7d176a7fa08d8f34f8fff6778fe622
Mathieu <mathieu.ancelin@gmail.com>
2016-02-01 21:16:13 +0100

    Handle transform

--- demo/index.js
+++ demo/index.js
@@ -1,31 +1,71 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Value, AnimatedDiv, animate } from '../src/index';
+import { AnimatedValue, AnimatedDiv, animate } from '../src/index';
 
 const App = React.createClass({
   getInitialState() {
     return {
-      anim: new Value(0),
+      flip1: false,
+      flip2: false,
+      anim1: new AnimatedValue(0),
+      anim2: new AnimatedValue(0),
+      anim3: new AnimatedValue(1.0),
     };
   },
-  handleClick() {
-    animate(this.state.anim, { toValue: 400 }).start();
+  run1() {
+    if (this.state.flip1) {
+      animate(this.state.anim1, { toValue: 0 }, () => this.setState({ flip1: !this.state.flip1 })).start();
+    } else {
+      animate(this.state.anim1, { toValue: 600 }, () => this.setState({ flip1: !this.state.flip1 })).start();
+    }
+  },
+  run2() {
+    if (this.state.flip2) {
+      animate(this.state.anim2, { toValue: 0, steps: 60 }, () => this.setState({ flip2: !this.state.flip2 })).start();
+    } else {
+      animate(this.state.anim2, { toValue: 600, steps: 60 }, () => this.setState({ flip2: !this.state.flip2 })).start();
+    }
+  },
+  reset() {
+    this.state.anim1.reset();
+    this.state.anim2.reset();
+  },
+  go() {
+    animate(this.state.anim2, { toValue: 600, steps: 60 }).start();
+  },
+  back() {
+    animate(this.state.anim2, { toValue: 0, steps: 60 }).start();
+  },
+  handleMouseDown() {
+    animate(this.state.anim3, { toValue: 0.8, steps: 20 }).start();
+  },
+  handleMouseUp() {
+    animate(this.state.anim3, { toValue: 1.0, steps: 20 }).start();
   },
   render() {
     return (
-      <div>
+      <div style={{ display: 'flex', flexDirection: 'column' }}>
         <div style={{ position: 'relative', height: 60 }}>
-          <AnimatedDiv onClick={this.handleClick} style={{
-            left: this.state.anim,
-            position: 'absolute',
-            display: 'inline-block',
-            border: '1px solid red',
-            width: 40,
-            height: 40 }}>
-            Click
+          <AnimatedDiv style={{ left: this.state.anim1, position: 'absolute', display: 'inline-block' }}>
+            <button type="button" style={{ width: 40, height: 40 }} onClick={this.run1}>Click</button>
           </AnimatedDiv>
         </div>
-        <button type="button" onClick={() => this.state.anim.reset()}>Reset</button>
+        <div style={{ position: 'relative', height: 60 }}>
+          <AnimatedDiv style={{ left: this.state.anim2, position: 'absolute', display: 'inline-block' }}>
+            <button type="button" style={{ width: 40, height: 40 }} onClick={this.run2}>Click</button>
+          </AnimatedDiv>
+        </div>
+        <AnimatedDiv
+          style={{ width: 40, height: 40, border: '1px solid red', transform: [{ scale: this.state.anim3 }] }}
+          onMouseDown={this.handleMouseDown}
+          onMouseUp={this.handleMouseUp}>
+          Click
+        </AnimatedDiv>
+        <div style={{ display: 'flex', flexDirection: 'row' }}>
+          <button type="button" onClick={this.reset}>Reset</button>
+          <button type="button" onClick={this.go}>go</button>
+          <button type="button" onClick={this.back}>back</button>
+        </div>
       </div>
     );
   },
--- src/index.js
+++ src/index.js
@@ -3,11 +3,57 @@ import React from 'react';
 import invariant from 'invariant';
 import easing from 'easing';
 
+// TODO : issue with multiple clics ...
+
+const noPx = [
+  'animationIterationCount',
+  'boxFlex',
+  'boxFlexGroup',
+  'boxOrdinalGroup',
+  'columnCount',
+  'fillOpacity',
+  'flex',
+  'flexGrow',
+  'flexPositive',
+  'flexShrink',
+  'flexNegative',
+  'flexOrder',
+  'fontWeight',
+  'lineClamp',
+  'lineHeight',
+  'opacity',
+  'order',
+  'orphans',
+  'stopOpacity',
+  'strokeDashoffset',
+  'strokeOpacity',
+  'strokeWidth',
+  'tabSize',
+  'widows',
+  'zIndex',
+  'zoom',
+];
+
 function isObject(χ) {
   return !!χ && (typeof χ === 'object' || typeof χ === 'function');
 }
 
-export function Value(v) {
+function extractAnimatedValues(arr) {
+  const values = [];
+  arr.forEach(item => {
+    for (const k in item) {
+      if (item.hasOwnProperty(k)) {
+        const tValue = item[k];
+        if (tValue.__animatedvalue) {
+          return values.push(tValue);
+        }
+      }
+    }
+  });
+  return values;
+}
+
+export function AnimatedValue(v) {
   const listeners = [];
   let value = v;
   const initValue = v;
@@ -42,40 +88,52 @@ export function Value(v) {
 }
 
 export function animate(value, options, callback) {
+  function stop() {
+    const { animationId, cb } = value.currentAnimation() || {};
+    window.cancelAnimationFrame(animationId);
+    if (cb) cb();
+  }
   invariant(options.hasOwnProperty('toValue'), 'You should specify a toValue options');
-  window.cancelAnimationFrame(value.currentAnimation());
+  stop();
   const startValue = value.getValue();
   const stopValue = options.toValue;
   const steps = options.steps || 100;
   const shape = options.shape || 'quadratic';
+  const values = options.values || easing(steps, shape, { endToEnd: true });
   const stepWidth = Math.abs(stopValue - startValue) / steps;
-  const values = easing(steps, shape, { endToEnd: true });
-  const easingRatio = 100 / values.reduce((a, b) => a + b);
+  const easingRatio = steps / values.reduce((a, b) => a + b);
   let index = 0;
+
   function nextStep(val, from, to, idx) {
+    if (to < from) {
+      return val.getValue() - ((stepWidth * values[idx]) * easingRatio);
+    }
     return val.getValue() + ((stepWidth * values[idx]) * easingRatio);
   }
   function update() {
     const newValue = (options.nextStep || nextStep)(value, startValue, stopValue, index, options);
     index += 1;
-    if (newValue < stopValue) {
+    if (stopValue < startValue && newValue > stopValue) {
       value.setValue(newValue);
       const requestID = window.requestAnimationFrame(update);
-      value.currentAnimation(requestID);
+      value.currentAnimation({ animationId: requestID, cb: callback });
+    } else if (stopValue >= startValue && newValue <= stopValue) {
+      value.setValue(newValue);
+      const requestID = window.requestAnimationFrame(update);
+      value.currentAnimation({ animationId: requestID, cb: callback });
     } else {
       value.setValue(stopValue);
-      if (callback) {
-        callback();
-      }
+      stop();
     }
   }
   return {
     start() {
       update();
+      return {
+        stop,
+      };
     },
-    stop() {
-      window.cancelAnimationFrame(value.currentAnimation());
-    },
+    stop,
   };
 }
 
@@ -105,6 +163,21 @@ export const AnimatedDiv = React.createClass({
           const value = what[key];
           if (value.__animatedvalue) {
             what[key] = value.getValue();
+          } else if (key === 'transform') {
+            what[key] = value.map(transform => {
+              let result = '';
+              for (const k in transform) {
+                if (transform.hasOwnProperty(k)) {
+                  const tValue = transform[k];
+                  if (tValue.__animatedvalue) {
+                    result += `${k}(${tValue.getValue()})`;
+                  } else {
+                    result += `${k}(${tValue})`;
+                  }
+                }
+              }
+              return result;
+            }).join(' ');
           } else if (isObject(value)) {
             infect(value);
           } else if (Array.isArray(value)) {
@@ -117,26 +190,43 @@ export const AnimatedDiv = React.createClass({
     return newStyle;
   },
   listenToChanges(name, path, value) {
-    const style = this.ref.style;
-    let current = style;
-    // TODO : handle complex imbrication
-    for (const key in path) {
-      if (path.hasOwnProperty(key)) {
-        current = current[path[key]];
+    this.ref.style[name] = `${value}${noPx.indexOf(name) > -1 ? '' : 'px'}`;
+  },
+  listenToChangesInTransform(transformArray) {
+    this.ref.style.transform = transformArray.map(transform => {
+      let result = '';
+      for (const k in transform) {
+        if (transform.hasOwnProperty(k)) {
+          const tValue = transform[k];
+          if (tValue.__animatedvalue) {
+            result += `${k}(${tValue.getValue()})`;
+          } else {
+            result += `${k}(${tValue})`;
+          }
+        }
       }
-    }
-    current[name] = `${value}px`; // TODO : handle px case
+      return result;
+    }).join(' ');
   },
   subscribe(what, path) {
     for (const key in what) {
       if (what.hasOwnProperty(key)) {
         const value = what[key];
-        if (value.__animatedvalue) {
-          this.unsubscribe.push(value.subscribe(this.listenToChanges.bind(this, key, path)));
-        } else if (isObject(value)) {
-          this.subscribe(value, [...path, key]);
-        } else if (Array.isArray(value)) {
-          this.subscribe(value, [...path, key]);
+        if (key === 'transform' && Array.isArray(value)) {
+          const valuesToListen = extractAnimatedValues(value);
+          if (valuesToListen.length > 0) {
+            valuesToListen.forEach(item => {
+              this.unsubscribe.push(item.subscribe(this.listenToChangesInTransform.bind(this, value)));
+            });
+          }
+        } else {
+          if (value.__animatedvalue) {
+            this.unsubscribe.push(value.subscribe(this.listenToChanges.bind(this, key, path)));
+          } else if (isObject(value)) {
+            this.subscribe(value, [...path, key]);
+          } else if (Array.isArray(value)) {
+            this.subscribe(value, [...path, key]);
+          }
         }
       }
     }
